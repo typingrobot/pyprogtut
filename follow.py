1b9c7e30c00adfe74f00a15d7f6bcfb7a2f13c57
Nick P <nick@devopsol.com>
2019-04-26 18:58:50 +0000

    generalize follow function as generator

--- follow.py
+++ follow.py
@@ -5,15 +5,21 @@
 import os
 import time
 
-f = open('/var/log/auth.log', 'r')
-f.seek(0, os.SEEK_END)
 
-while True:
-    line = f.readline()
-    if not line:
-        time.sleep(0.1)
-        continue
+def follow(filename):
 
+    f = open(filename, 'r')
+    f.seek(0, os.SEEK_END)
+
+    while True:
+        line = f.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+for line in follow('/var/log/auth.log'):
     if 'opened' in line:
         info = line.split(':')[-1].split('by')
         print('Session opened for user: {} by {}'.format(
