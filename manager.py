6e1ffd49f5c5e77daa3ca00a7d999b95758f976f
Nick P <nick@devopsol.com>
2019-05-16 22:16:22 +0000

    simple example of custom context manager for resource handling

---
+++ manager.py
@@ -0,0 +1,10 @@
+
+
+class Manager():
+    def __enter__(self):
+        print('Entering')
+        return 'some value'
+
+    def __exit__(self, ty, val, tb):
+        print('Exiting')
+        print(ty, val, tb)
